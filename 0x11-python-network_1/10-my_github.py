3d94311c629081ae6eb1c1be6b062bfd42e7fcc4
Obed45 <adjeijason68@gmail.com>
2023-07-01 04:19:17 -0700

    a??

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,20 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    if len(sys.argv) != 3:
+        print("Usage: python script.py username password")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    auth = HTTPBasicAuth(username, password)
+
+    try:
+        response = requests.get("https://api.github.com/user", auth=auth)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+        user_id = response.json().get("id")
+        print(user_id)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
